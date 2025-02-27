c0528f0f8808a59b297863f7bf77955f7aec88f6
subin75 <wannak_d1210@naver.com>
2025-02-27 11:28:51 +0900

    sdfsdf

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './page/Home';
+import './Style.scss';
+import { BrowserRouter, Router, Route, NavLink, Routes} from 'react-router-dom'
+import StateProps from './page/StateProps';
+import Deploy from './page/Deploy';
+import Css from './page/Css';
+
+
+function App() {
+  return (
+    <BrowserRouter basename='/react_tutorial'>
+     <header>
+      <NavLink to ="/">HOME</NavLink>
+      <NavLink to="/css">CSS</NavLink>
+      <NavLink to="/router">Router</NavLink>
+      <NavLink to="/stateprops">State&Props</NavLink>
+      <NavLink to="/deploy">Deploy</NavLink>
+     </header>
+
+     <main>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/css" element={<Css/>} />
+        <Route path="/router" element={<Router/>} />
+        <Route path="/stateprops" element={<StateProps/>} />
+        <Route path="/deploy" element={<Deploy/>} />
+      </Routes>
+     </main>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/page/StateProps.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function StateProps() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default StateProps
